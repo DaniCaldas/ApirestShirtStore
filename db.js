14bb9a4368e0b7f87ff0c8a1b5092f79f7a9c957
Daniel Caldas <daniel.caldasgoncalves@gmail.com>
2022-07-22 14:41:07 -0300

    atualizando erro no servidor

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Carrinho = require('./data/Carrinho.json')
 const Select = require('./data/Seject.json')
 const Plataformas = require('./data/Plataformas.json')
-const Camisas = require('.data/Camisas.json')
+const Camisas = require('./data/Camisas.json')
 
 module.exports ={
     Carrinho,
