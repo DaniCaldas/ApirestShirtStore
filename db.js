fae867a2a42776e5fbab006e2ca0a87b7e00870c
Daniel Caldas <daniel.caldasgoncalves@gmail.com>
2022-07-22 17:45:13 -0300

    adicionando novos items

--- db.js
+++ db.js
@@ -2,10 +2,14 @@ const Carrinho = require('./data/Carrinho.json')
 const Select = require('./data/Seject.json')
 const Plataformas = require('./data/Plataformas.json')
 const Camisas = require('./data/Camisas.json')
+const Camisas_polo = require('./data/Camisas_polo.json')
+const Corta_Vento = require('./data/Corta_Vento.json')
 
 module.exports ={
     Carrinho,
     Select,
     Plataformas,
-    Camisas
+    Camisas,
+    Camisas_polo,
+    Corta_Vento
 }
