8bf120082ae8c4ebc66a19f4d096f96146ebac4a
Daniel Caldas <daniel.caldasgoncalves@gmail.com>
2022-07-22 13:12:24 -0300

    atualizando api camisas

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const Carrinho = require('./data/Carrinho.json')
 const Select = require('./data/Seject.json')
 const Plataformas = require('./data/Plataformas.json')
+const Camisas = require('.data/Camisas.json')
 
 module.exports ={
     Carrinho,
     Select,
-    Plataformas
+    Plataformas,
+    Camisas
 }
